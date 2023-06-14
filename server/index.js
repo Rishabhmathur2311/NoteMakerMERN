a59881a39423ef864c75b3741e6740e943d3895d
Rishabh Mathur <rishabhmathur2311@gmail.com>
2023-06-14 20:38:52 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import Route from "./routes/route.js";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import * as path from 'path';
 dotenv.config();
 const app=express();
 
@@ -11,6 +12,22 @@ app.use(cors());
 app.use(bodyParser.json({exended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', Route);
+
+//----------------------DEPLOYMENT
+
+const __dirname1=path.resolve();
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname1,"..", "/client/build")));
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname1,"..", "client", "build", "index.html"));
+    })
+}else{
+    app.get('/', (req, res)=>{
+        res.send("API is running successfully");
+    })
+}
+
+//--------------------DEPLOYMENT
 
 Connection();
 
